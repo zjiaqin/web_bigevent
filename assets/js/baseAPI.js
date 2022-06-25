c0703ab6be93be1cdcac0a630857e26c71c5b2b6
qinzj <zjia.qin@outlook.com>
2022-06-25 14:16:19 +0800

    完成主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,19 @@
     $.ajaxPrefilter(function(options) {
         // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
         options.url = 'http://www.liulongbin.top:3007' + options.url
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+        // 全局挂载complete回调函数，判断身份是否验证成功，若失败跳转回登录界面并删除本地存储中token数据
+        options.complete = function(res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                // 清除本地存储token
+                localStorage.removeItem('token')
+                    // 跳转至登录页面
+                location.href = '/login.html'
+            }
+        }
     })
 })()
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+;
+(function() {
+    // 调用获取用户信息的函数
+    getUserinfo()
+
+    // 绑定点击退出事件
+    let layer = layui.layer
+    $('#logout').click(function() {
+        layer.confirm('您要退出登录吗?', { icon: 3, title: '提示' }, function(index) {
+            // 清除本地存储token
+            localStorage.removeItem('token')
+                // 关闭弹出框
+            layer.close(index);
+            // 跳转至登录页面
+            location.href = '/login.html'
+
+
+        });
+
+    })
+
+
+    // 封装获取用户信息的函数
+    function getUserinfo() {
+        $.get('/my/userinfo', function(res) {
+            if (res.status !== 0) {
+                return layer.msg('获取用户信息失败！')
+            }
+            renderAvatar(res.data)
+        })
+    }
+    // 封装渲染用户头像函数
+    function renderAvatar(user) {
+        let name = user.nickname || user.username
+        $('#welcome').html(`欢迎&nbsp;&nbsp;${name}`)
+            // 无图片头像是采用文字头像
+        if (user.user_pic === null) {
+            let first = name[0].toUpperCase()
+            $('.layui-nav-img').hide()
+            $('.text-avatar').html(first).show()
+        } else {
+            // 有图片头像时使用图片头像
+            $('.text-avatar').hide()
+            $('.layui-nav-img').attr('src', user.user_pic).show()
+        }
+    }
+})()
